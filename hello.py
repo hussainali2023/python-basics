aaf59394558d66deebbfb081a432607f97089ead
Hussain Ali <iamhussainali2@gmail.com>
2024-05-14 15:29:23 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+def chai(n):
+    print(n)
+
+chai(4)
+
+def addition(a, b):
+    sum = a + b
+    print(sum)
+
+addition(4, 5)
+
+chai_one = "lemon tea"
+chai_two = "masala chai"
+chai_three = "ginger tea"
