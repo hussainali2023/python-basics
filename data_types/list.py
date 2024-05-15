ce416342e40be6f903a7b1e3b9bf57ebe798e431
Hussain Ali <iamhussainali2@gmail.com>
2024-05-15 21:39:32 +0530

    added some list example

--- data_types/list.py
+++ data_types/list.py
@@ -8,3 +8,33 @@ mylist[1] = "World"
 
 print(mylist[1])
 
+myListOne = [1, 2, 3]
+myListTwo = myListOne
+# myListOne = "chai"
+
+myListOne[0] = 32
+
+print(myListOne)
+print(myListTwo)
+
+l1 = [1, 2, 3]
+l2 = l1
+
+print(l1)
+print(l2)
+
+l1[0] = 33
+
+print(l1)
+print(l2)
+
+a1 = [1, 2, 3]
+a2 = a1
+a2 = [1, 2, 3]
+
+a1[0] = 22
+
+print(a2)
+
+b1 = [1, 2, 3]
+b2 = b1[:] # copy of b1
---
+++ data_types/tuple.py
@@ -0,0 +1,14 @@
+myTup = (1, 2, 3)
+
+# print(myTup[3]) IndexError: tuple index out of range 
+
+print(myTup[2])
+
+# myTup[3] = 4
+
+#print(myTup) #TypeError: 'tuple' object does not support item assignment
+
+
+
+
+
